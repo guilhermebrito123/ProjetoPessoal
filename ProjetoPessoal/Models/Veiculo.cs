21043da31542f39a5f10782caac15495fad10d66
guilhermebrito123 <gbfsilva@sga.pucminas.br>
2024-07-04 16:01:15 -0300

    Configuração do BD e da tabela Veiculo

---
+++ ProjetoPessoal/Models/Veiculo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetoPessoal.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="O campo Nome é obrigatório!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo Placa é obrigatório!")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "O campo AnoFabricacao é obrigatório!")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "O campo AnoModelo é obrigatório!")]
+        public int AnoModelo { get; set; }
+    }
+}
